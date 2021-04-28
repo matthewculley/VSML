6745e8807c3ed880597e30a11928ad60510c9326
matthewculley <42278387+matthewculley@users.noreply.github.com>
2021-04-28 02:33:16 +0100

    Add files via upload

---
+++ VSML - Copy/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace VSML
+{
+    partial class VSMLProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(VSMLProgram));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.MainText = new System.Windows.Forms.RichTextBox();
+            this.AuxText = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.OpenFileBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.SaveBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.SaveAsBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator5 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExportXmlBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator6 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExportHtmlBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator7 = new System.Windows.Forms.ToolStripSeparator();
+            this.ExportDocxBtn = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator8 = new System.Windows.Forms.ToolStripSeparator();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.ZoomOut = new System.Windows.Forms.ToolStripButton();
+            this.ZoomIn = new System.Windows.Forms.ToolStripButton();
+            this.ZoomLabel = new System.Windows.Forms.ToolStripLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            resources.ApplyResources(this.splitContainer1, "splitContainer1");
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            resources.ApplyResources(this.splitContainer1.Panel1, "splitContainer1.Panel1");
+            this.splitContainer1.Panel1.Controls.Add(this.MainText);
+            // 
+            // splitContainer1.Panel2
+            // 
+            resources.ApplyResources(this.splitContainer1.Panel2, "splitContainer1.Panel2");
+            this.splitContainer1.Panel2.Controls.Add(this.AuxText);
+            // 
+            // MainText
+            // 
+            resources.ApplyResources(this.MainText, "MainText");
+            this.MainText.DetectUrls = false;
+            this.MainText.Name = "MainText";
+            // 
+            // AuxText
+            // 
+            resources.ApplyResources(this.AuxText, "AuxText");
+            this.AuxText.DetectUrls = false;
+            this.AuxText.Name = "AuxText";
+            // 
+            // toolStrip1
+            // 
+            resources.ApplyResources(this.toolStrip1, "toolStrip1");
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.OpenFileBtn,
+            this.toolStripSeparator3,
+            this.SaveBtn,
+            this.toolStripSeparator4,
+            this.SaveAsBtn,
+            this.toolStripSeparator5,
+            this.ExportXmlBtn,
+            this.toolStripSeparator6,
+            this.ExportHtmlBtn,
+            this.toolStripSeparator7,
+            this.ExportDocxBtn,
+            this.toolStripSeparator8,
+            this.ZoomIn,
+            this.ZoomLabel,
+            this.ZoomOut});
+            this.toolStrip1.Name = "toolStrip1";
+            // 
+            // OpenFileBtn
+            // 
+            resources.ApplyResources(this.OpenFileBtn, "OpenFileBtn");
+            this.OpenFileBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.OpenFileBtn.Name = "OpenFileBtn";
+            this.OpenFileBtn.Click += new System.EventHandler(this.OpenFileBtn_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            resources.ApplyResources(this.toolStripSeparator3, "toolStripSeparator3");
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            // 
+            // SaveBtn
+            // 
+            resources.ApplyResources(this.SaveBtn, "SaveBtn");
+            this.SaveBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.SaveBtn.Name = "SaveBtn";
+            this.SaveBtn.Click += new System.EventHandler(this.SaveBtn_Click);
+            // 
+            // toolStripSeparator4
+            // 
+            resources.ApplyResources(this.toolStripSeparator4, "toolStripSeparator4");
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            // 
+            // SaveAsBtn
+            // 
+            resources.ApplyResources(this.SaveAsBtn, "SaveAsBtn");
+            this.SaveAsBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.SaveAsBtn.Name = "SaveAsBtn";
+            this.SaveAsBtn.Click += new System.EventHandler(this.SaveAsBtn_Click);
+            // 
+            // toolStripSeparator5
+            // 
+            resources.ApplyResources(this.toolStripSeparator5, "toolStripSeparator5");
+            this.toolStripSeparator5.Name = "toolStripSeparator5";
+            // 
+            // ExportXmlBtn
+            // 
+            resources.ApplyResources(this.ExportXmlBtn, "ExportXmlBtn");
+            this.ExportXmlBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ExportXmlBtn.Name = "ExportXmlBtn";
+            this.ExportXmlBtn.Click += new System.EventHandler(this.ExportXmlBtn_Click);
+            // 
+            // toolStripSeparator6
+            // 
+            resources.ApplyResources(this.toolStripSeparator6, "toolStripSeparator6");
+            this.toolStripSeparator6.Name = "toolStripSeparator6";
+            // 
+            // ExportHtmlBtn
+            // 
+            resources.ApplyResources(this.ExportHtmlBtn, "ExportHtmlBtn");
+            this.ExportHtmlBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ExportHtmlBtn.Name = "ExportHtmlBtn";
+            this.ExportHtmlBtn.Click += new System.EventHandler(this.ExportHtmlBtn_Click);
+            // 
+            // toolStripSeparator7
+            // 
+            resources.ApplyResources(this.toolStripSeparator7, "toolStripSeparator7");
+            this.toolStripSeparator7.Name = "toolStripSeparator7";
+            // 
+            // ExportDocxBtn
+            // 
+            resources.ApplyResources(this.ExportDocxBtn, "ExportDocxBtn");
+            this.ExportDocxBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ExportDocxBtn.Name = "ExportDocxBtn";
+            this.ExportDocxBtn.Click += new System.EventHandler(this.ExportDocxBtn_Click);
+            // 
+            // toolStripSeparator8
+            // 
+            resources.ApplyResources(this.toolStripSeparator8, "toolStripSeparator8");
+            this.toolStripSeparator8.Name = "toolStripSeparator8";
+            // 
+            // panel1
+            // 
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Controls.Add(this.splitContainer1);
+            this.panel1.Name = "panel1";
+            // 
+            // ZoomOut
+            // 
+            resources.ApplyResources(this.ZoomOut, "ZoomOut");
+            this.ZoomOut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ZoomOut.Name = "ZoomOut";
+            this.ZoomOut.Click += new System.EventHandler(this.ZoomOut_Click);
+            // 
+            // ZoomIn
+            // 
+            resources.ApplyResources(this.ZoomIn, "ZoomIn");
+            this.ZoomIn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.ZoomIn.Name = "ZoomIn";
+            this.ZoomIn.Click += new System.EventHandler(this.ZoomIn_Click);
+            // 
+            // ZoomLabel
+            // 
+            resources.ApplyResources(this.ZoomLabel, "ZoomLabel");
+            this.ZoomLabel.Name = "ZoomLabel";
+            // 
+            // VSMLProgram
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "VSMLProgram";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton OpenFileBtn;
+        private System.Windows.Forms.ToolStripButton SaveBtn;
+        private System.Windows.Forms.ToolStripButton SaveAsBtn;
+        private System.Windows.Forms.ToolStripButton ExportXmlBtn;
+        private System.Windows.Forms.ToolStripButton ExportHtmlBtn;
+        private System.Windows.Forms.ToolStripButton ExportDocxBtn;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox MainText;
+        private System.Windows.Forms.RichTextBox AuxText;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator5;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator6;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator7;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator8;
+        private System.Windows.Forms.ToolStripButton ZoomIn;
+        private System.Windows.Forms.ToolStripLabel ZoomLabel;
+        private System.Windows.Forms.ToolStripButton ZoomOut;
+    }
+}
+
---
+++ VSML - Copy/Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VSML
+{
+    public partial class VSMLProgram : Form
+    {
+        private string fileName = "";
+
+
+        public VSMLProgram()
+        {
+            InitializeComponent();
+            this.Text = "VSML - New File";
+            ZoomLabel.Text = "Zoom: " + MainText.ZoomFactor.ToString();
+        }
+
+        private void OpenFileBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openfile = new OpenFileDialog();
+            openfile.Title = "Open file:";
+            openfile.Filter = "Text files (*.vsml)| *.vsml";
+            openfile.RestoreDirectory = true;
+            if (openfile.ShowDialog() == DialogResult.OK)
+            {
+                MainText.Clear();
+                using (StreamReader sr = new StreamReader(openfile.FileName))
+                {
+                    MainText.Text = sr.ReadToEnd();
+                    var t = MainText.Text;
+                    MainText.Text = t;
+                    sr.Close();
+                }
+            }
+            this.fileName = openfile.FileName;
+            this.Text = "VSML - " + Path.GetFileName(this.fileName);
+        }
+
+        private void SaveBtn_Click(object sender, EventArgs e)
+        {
+            // If empty filename
+            if (this.fileName == "")
+            {
+                // Use save as functionality
+                this.SaveAsBtn_Click(sender, e);
+            }
+            else
+            {
+                // Try and save the file
+                try
+                {
+                    StreamWriter txtoutput = new StreamWriter(this.fileName);
+                    txtoutput.Write(MainText.Text);
+                    txtoutput.Close();
+                }
+                catch (System.ArgumentException)
+                {
+                    return;
+                }
+            }
+        }
+
+        private void SaveAsBtn_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog savefile = new SaveFileDialog();
+            savefile.Title = "Save file as..";
+            savefile.Filter = "Text files (*.vsml)| *.vsml";
+            if (savefile.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter txtoutput = new StreamWriter(savefile.FileName);
+                txtoutput.Write(MainText.Text);
+                txtoutput.Close();
+            }
+            this.fileName = savefile.FileName;
+            this.Text = "VSML - " + Path.GetFileName(this.fileName);
+        }
+
+        private void ExportXmlBtn_Click(object sender, EventArgs e)
+        {
+            VsmlReader reader = new VsmlReader();
+            var toXml = reader.VsmlToXml(MainText.Text);
+            if (toXml.Item1)    //if successful and no errors
+            {
+                SaveFileDialog savefile = new SaveFileDialog
+                {
+                    Title = "Save file as..",
+                    Filter = "XML Files|*.xml"
+                };
+
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter txtoutput = new StreamWriter(savefile.FileName);
+                    txtoutput.Write(reader.Document.OuterXml);
+                    txtoutput.Close();
+                }
+                AuxText.Text = "Exported as XML to " + savefile.FileName + "\nExported content:\n" + reader.Document.OuterXml;
+            }
+            else
+                AuxText.Text = "Error on line " + toXml.Item2.Item1 + " of the XML " + toXml.Item2.Item2 + reader.Document.OuterXml;
+        }
+
+        private void ExportHtmlBtn_Click(object sender, EventArgs e)
+        {
+            VsmlReader reader = new VsmlReader();
+            VsmlWriter writer = new VsmlWriter(reader.Document);
+            var toXml = reader.VsmlToXml(MainText.Text);
+            if (toXml.Item1)    //if successful and no errors
+            {
+                SaveFileDialog savefile = new SaveFileDialog
+                {
+                    Title = "Save file as..",
+                    Filter = "HTML Files|*.html"
+                };
+
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter txtoutput = new StreamWriter(savefile.FileName);
+                    txtoutput.Write(writer.ToHtml());
+                    txtoutput.Close();
+                }
+                AuxText.Text = "Exported as HTML to " + savefile.FileName + "\nExported content:\n" + writer.ToHtml();
+            }
+            else
+                AuxText.Text = "Error on line " + toXml.Item2.Item1 + " of the XML " + toXml.Item2.Item2 + reader.Document.OuterXml;
+        }
+
+
+        public void ExportDocxBtn_Click(object sender, EventArgs e)
+        {
+            VsmlReader reader = new VsmlReader();
+            VsmlWriter writer = new VsmlWriter(reader.Document);
+            var toXml = reader.VsmlToXml(MainText.Text);
+            if (toXml.Item1)    //if successful and no errors
+            {
+                SaveFileDialog savefile = new SaveFileDialog
+                {
+                    Title = "Save file as..",
+                    Filter = "Docx Files|*.Docx"
+                };
+
+                if (savefile.ShowDialog() == DialogResult.OK)
+                {
+                    var success = writer.ToDocx(savefile.FileName);
+                    if (success.Item1)
+                        AuxText.Text = "Exported .Docx version of XML to " + savefile.FileName + "\nExported content:\n" + reader.Document.OuterXml;
+                    else
+                        AuxText.Text = "Error: " + success.Item2;
+                }
+            }
+            else
+                AuxText.Text = "Error on line " + toXml.Item2.Item1 + " of the XML " + toXml.Item2.Item2 + reader.Document.OuterXml;
+        }
+
+        // Key shortcuts
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.Control & e.KeyCode == Keys.O)
+            {
+                this.OpenFileBtn_Click(sender, e);
+                e.Handled = true;
+            }
+            if (e.Control & e.KeyCode == Keys.S)
+            {
+                this.SaveBtn_Click(sender, e);
+                e.Handled = true;
+            }
+        }
+
+        private void ZoomIn_Click(object sender, EventArgs e)
+        {
+            float zoom = MainText.ZoomFactor;
+            if (zoom < 63)
+            {
+                MainText.ZoomFactor += (float)0.25;
+            }
+            ZoomLabel.Text = "Zoom: " + MainText.ZoomFactor.ToString();
+        }
+
+        private void ZoomOut_Click(object sender, EventArgs e)
+        {
+            float zoom = MainText.ZoomFactor;
+            if (zoom > 1)
+            {
+                MainText.ZoomFactor -= (float).25;
+            }
+            ZoomLabel.Text = "Zoom: " + MainText.ZoomFactor.ToString();
+        }
+    }
+}
